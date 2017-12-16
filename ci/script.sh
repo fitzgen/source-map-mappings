cd595f8a382554a530c944d233d87d6645c8a627
Nick Fitzgerald <fitzgen@gmail.com>
2017-12-16 11:29:13 -0600

    Fix CI

---
+++ ci/script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+
+case "$JOB" in
+    "test")
+        cargo install -f cargo-readme
+        cargo test
+        ;;
+    "bench")
+        cargo bench
+        ;;
+    "wasm")
+        rustup target add wasm32-unknown-unknown
+        cargo build --target wasm32-unknown-unknown
+        ;;
+    *)
+        echo "Unknown \$JOB = '$JOB'"
+        exit 1
+esac
