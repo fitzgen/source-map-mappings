8e37cbf23bb4a20d9f4991a976d8da7fc136eb16
Nick Fitzgerald <fitzgen@gmail.com>
2017-12-19 12:57:24 -0800

    C API -> Wasm API and some Cargo.toml things

--- ci/script.sh
+++ ci/script.sh
@@ -12,14 +12,14 @@ case "$JOB" in
         ;;
     "wasm")
         rustup target add wasm32-unknown-unknown
-        cd source-map-mappings-c-api/
+        cd source-map-mappings-wasm-api/
 
         # Non-release builds are broken for wasm32-unknown-unknown targets right now.
         # cargo build --target wasm32-unknown-unknown
-        # test -f target/wasm32-unknown-unknown/debug/source_map_mappings_c_api.wasm
+        # test -f target/wasm32-unknown-unknown/debug/source_map_mappings_wasm_api.wasm
 
         cargo build --release --target wasm32-unknown-unknown
-        test -f target/wasm32-unknown-unknown/release/source_map_mappings_c_api.wasm
+        test -f target/wasm32-unknown-unknown/release/source_map_mappings_wasm_api.wasm
         ;;
     *)
         echo "Unknown \$JOB = '$JOB'"
