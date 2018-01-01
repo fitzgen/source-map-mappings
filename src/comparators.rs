4679d3e1eb868d598b45a42299cec464f27ecc36
Nick Fitzgerald <fitzgen@gmail.com>
2018-01-01 09:14:13 -0800

    Mark comparator functions as `#[inline]`

--- src/comparators.rs
+++ src/comparators.rs
@@ -10,6 +10,7 @@ impl<T, F> ComparatorFunction<Option<T>> for F
 where
     F: ComparatorFunction<T>,
 {
+    #[inline]
     fn compare(a: &Option<T>, b: &Option<T>) -> Ordering {
         match (a, b) {
             (&None, &None) => Ordering::Equal,
@@ -24,6 +25,7 @@ where
 pub struct ByGeneratedLocation;
 
 impl ComparatorFunction<Mapping> for ByGeneratedLocation {
+    #[inline]
     fn compare(a: &Mapping, b: &Mapping) -> Ordering {
         a.generated_line
             .cmp(&b.generated_line)
@@ -36,6 +38,7 @@ impl ComparatorFunction<Mapping> for ByGeneratedLocation {
 pub struct ByOriginalLocation;
 
 impl ComparatorFunction<Mapping> for ByOriginalLocation {
+    #[inline]
     fn compare(a: &Mapping, b: &Mapping) -> Ordering {
         ByOriginalLocation::compare(&a.original, &b.original).then(
             a.generated_line
@@ -46,6 +49,7 @@ impl ComparatorFunction<Mapping> for ByOriginalLocation {
 }
 
 impl ComparatorFunction<OriginalLocation> for ByOriginalLocation {
+    #[inline]
     fn compare(a: &OriginalLocation, b: &OriginalLocation) -> Ordering {
         a.source
             .cmp(&b.source)
